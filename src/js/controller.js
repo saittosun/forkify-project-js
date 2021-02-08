6338c8c6a467799212a7be380f928fe4cfe0fe23
tostos <saittosun2@gmail.com>
2021-02-08 13:19:47 +0100

    search is finished

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,12 +2,18 @@ import { async } from 'regenerator-runtime/runtime';
 import * as model from './model'
 import recipeView from './views/recipeView';
 import searchView from './views/searchView';
+import resultsView from './views/resultsView';
 
 //add polyfills for es6 features  
 // this one is for polyfilling everything else
 import 'core-js/stable';
 // this one is for polyfilling async/await
 import 'regenerator-runtime/runtime';
+
+// this is not real js this is coming from parcel
+if (module.hot) {
+  module.hot.accept()
+}
 
 const controlRecipes = async function () {
   try {
@@ -31,10 +37,16 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+    console.log(resultsView)
     const query = searchView.getQuery();
     if (!query) return;
 
-    await model.loadSearchResults(query)
+    await model.loadSearchResults(query);
+
+    // 3) render results
+    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results)
   } catch (err) {
     console.log(err);
   }
