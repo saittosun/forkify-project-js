803c4cb6ce64e6cb6111d9880eba2625025c987e
tostos <saittosun2@gmail.com>
2021-02-09 12:57:04 +0100

    developing a dom updating algoritm

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,9 +2,9 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
   _data;
+
   render(data) {
     if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
-
 
     this._data = data;
     const markup = this._generateMarkup();
@@ -14,6 +14,37 @@ export default class View {
 
   _clear() {
     this._parentElement.innerHTML = ''
+  }
+
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    console.log(newElements);
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'))
+    console.log(curElements)
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      console.log(curEl, newEl.isEqualNode(curEl))
+
+      // updates changed TEXT
+      if (!newEl.isEqualNode(curEl) && newEl.firstChild?.nodeValue.trim()) {
+        console.log(newEl)
+        console.log(newEl.firstChild.nodeValue.trim())
+        curEl.textContent = newEl.textContent;
+      }
+
+      // updates changed ATTRIBUTES
+      if (!newEl.isEqualNode(curEl)) {
+        console.log(Array.from(newEl.attributes));
+        Array.from(newEl.attributes).forEach(attr => {
+          curEl.setAttribute(attr.name, attr.value)
+        })
+      }
+    })
   }
 
   renderSpinner() {
