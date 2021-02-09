ae457b4486374202d1569b8942c240f895b8114f
tostos <saittosun2@gmail.com>
2021-02-09 09:45:49 +0100

    pagination is finished

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,6 +9,7 @@ import resultsView from './views/resultsView';
 import 'core-js/stable';
 // this one is for polyfilling async/await
 import 'regenerator-runtime/runtime';
+import paginationView from './views/paginationView';
 
 // this is not real js this is coming from parcel
 // if (module.hot) {
@@ -39,24 +40,42 @@ const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
     console.log(resultsView)
+
+    // 1) get search query
     const query = searchView.getQuery();
     if (!query) return;
 
+    // 2) load search results
     await model.loadSearchResults(query);
 
     // 3) render results
     console.log(model.state.search.results);
     // resultsView.render(model.state.search.results)
     resultsView.render(model.getSearchResultsPage());
+
+    // 4) render initial pagination buttons
+    paginationView.render(model.state.search)
   } catch (err) {
     console.log(err);
   }
+}
+
+const controlPagination = function (goToPage) {
+  console.log('pagination controller')
+  console.log(goToPage)
+
+  // 1) render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage))
+
+  // 2) render NEW pagination buttons
+  paginationView.render(model.state.search)
 }
 
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination)
 }
 init();
 
